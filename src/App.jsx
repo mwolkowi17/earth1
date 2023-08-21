79e37dc814d4a55653862df960b2b1fbd6439eaf
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2023-08-21 16:12:18 +0200

    plane around

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, Environment, useGLTF, Stars, useTexture } from '@react-three/drei'
 import { Plane } from './Plane'
+import { DirectionalLight } from 'three'
 
 function Box(props) {
   // This reference gives us direct access to the THREE.Mesh object
@@ -32,13 +33,28 @@ function Box(props) {
 function EartSphere() {
   const texture = useTexture('2k_earth_daymap.jpg')
   const earthRef2 = useRef()
-  useFrame((state, delta) => (earthRef2.current.rotation.y += delta * 0.2))
+  useFrame((state, delta) => (earthRef2.current.rotation.y += delta * 0.1))
   return (
     <group ref={earthRef2}>
       <mesh scale={1.5}  >
         <sphereGeometry args={[1, 64, 64]} />
         <meshPhysicalMaterial map={texture} clearcoat={1} clearcoatRoughness={0} roughness={0} metalness={0.5} />
       </mesh>
+      {/* <Box position={[2, 0, 0]} /> */}
+      {/* <Plane position={[0, 0, 0]} /> */}
+    </group>
+  )
+}
+function PlaneMoving() {
+  const texture = useTexture('2k_earth_daymap.jpg')
+  const earthRef2 = useRef()
+  useFrame((state, delta) => (earthRef2.current.rotation.y += delta * 0.3))
+  return (
+    <group ref={earthRef2}>
+      {/* <mesh scale={1.5}  >
+        <sphereGeometry args={[0.2, 64, 64]} />
+        <meshStandardMaterial  />
+      </mesh> */}
       {/* <Box position={[2, 0, 0]} /> */}
       <Plane position={[0, 0, 0]} />
     </group>
@@ -48,10 +64,11 @@ function EartSphere() {
 export default function App() {
   return (
     <Canvas>
-      <Environment files="kloppenheim_02_puresky_1k_dark.hdr" background={true} blur={0.1} />
+      <ambientLight />
+      <Environment files="kloppenheim_02_puresky_1k_dark.hdr" background={true} blur={0.1} />     
       <EartSphere position={[0, 0, 0]} />
-      <OrbitControls
-      
+      <PlaneMoving position={[0,0,0]} />
+      <OrbitControls      
         minAzimuthAngle={-Math.PI / 4}
         maxAzimuthAngle={Math.PI / 4}
         minPolarAngle={Math.PI / 6}
--- src/Plane.jsx
+++ src/Plane.jsx
@@ -2,6 +2,6 @@ import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 export function Plane() {
-    const gltf = useLoader(GLTFLoader, '/samolot1.glb')
+    const gltf = useLoader(GLTFLoader, 'samolot1.glb')
     return <primitive object={gltf.scene} scale={.1}  position={[2,0,0]} rotation={[0,-Math.PI/2,0]}/>
   }
