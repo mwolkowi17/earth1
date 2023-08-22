75c62824012d8a74343354020650c3259f39e47c
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2023-08-22 21:35:34 +0200

    icons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
 
 import './App.css'
-import { useRef, useState } from 'react'
+import { useRef, Suspense } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, Environment, useGLTF, Stars, useTexture } from '@react-three/drei'
+import { OrbitControls, Environment, Loader, Stars, useTexture } from '@react-three/drei'
 import { Plane } from './Plane'
-import { DirectionalLight } from 'three'
-
-function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.y += delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[.3, .3, .3]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
+import { FaGitkraken, FaGrunt, FaMapMarkerAlt } from 'react-icons/fa'
+import { Marker } from './Marker'
 
 
 function EartSphere() {
@@ -39,6 +17,22 @@ function EartSphere() {
       <mesh scale={1.5}  >
         <sphereGeometry args={[1, 64, 64]} />
         <meshPhysicalMaterial map={texture} clearcoat={1} clearcoatRoughness={0} roughness={0} metalness={0.5} />
+        <Marker  rotation={[0, Math.PI / 2, 0]} position={[0, 1.3, 0]}>
+        <div style={{ position: 'absolute', fontSize: 14, letterSpacing: -0.5, left: 37.5, color: 'white' }}>north</div>
+          <FaMapMarkerAlt style={{ color: 'orange', scale:'3' }} />
+        </Marker>
+        <group position={[0, 0, 1.1]} rotation={[0, 0, Math.PI]}>
+          <Marker rotation={[0, Math.PI / 2, Math.PI / 2]}>
+          <div style={{ position: 'absolute', fontSize: 14, letterSpacing: -0.5, left: 37.5, color:'white' }}>kraken base</div>
+            <FaGitkraken style={{ color: 'indianred', scale:'3' }} />
+          </Marker>
+        </group>
+        <group position={[0, 0, -1.1]} rotation={[0, 0, Math.PI]}>
+          <Marker rotation={[0, Math.PI / 2, Math.PI / 2]}>
+          <div style={{ position: 'absolute', fontSize: 14, letterSpacing: -0.5, left: 37.5, color:'white' }}>corsar group</div>
+            <FaGrunt style={{ color: 'indianred', scale:'3' }} />
+          </Marker>
+        </group>
       </mesh>
       {/* <Box position={[2, 0, 0]} /> */}
       {/* <Plane position={[0, 0, 0]} /> */}
@@ -46,7 +40,7 @@ function EartSphere() {
   )
 }
 function PlaneMoving() {
-  const texture = useTexture('2k_earth_daymap.jpg')
+  
   const earthRef2 = useRef()
   useFrame((state, delta) => (earthRef2.current.rotation.y += delta * 0.3))
   return (
@@ -61,21 +55,28 @@ function PlaneMoving() {
   )
 }
 
+
+
 export default function App() {
   return (
-    <Canvas>
-      <ambientLight />
-      <Environment files="kloppenheim_02_puresky_1k_dark.hdr" background={true} blur={0.1} />     
-      <EartSphere position={[0, 0, 0]} />
-      <PlaneMoving position={[0,0,0]} />
-      <OrbitControls      
-        minAzimuthAngle={-Math.PI / 4}
-        maxAzimuthAngle={Math.PI / 4}
-        minPolarAngle={Math.PI / 6}
-        maxPolarAngle={Math.PI - Math.PI / 6} />
-      <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
-    </Canvas>
+    <>
+      <Canvas>
 
+        <ambientLight />
+        <Environment files="kloppenheim_02_puresky_1k_dark.hdr" background={true} blur={0.1} />
+        <Suspense fallback={null}>
+          <EartSphere position={[0, 0, 0]} />
+          <PlaneMoving position={[0, 0, 0]} />
+          <OrbitControls
+            minAzimuthAngle={-Math.PI / 4}
+            maxAzimuthAngle={Math.PI / 4}
+            minPolarAngle={Math.PI / 6}
+            maxPolarAngle={Math.PI - Math.PI / 6} />
+          <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   )
 }
 
