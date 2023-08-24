0e9e696f960046f7fc40e3d6a62f0c988dda2dc8
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2023-08-24 21:50:38 +0200

    rotation after event set

--- src/App.jsx
+++ src/App.jsx
@@ -13,26 +13,26 @@ import { dataText } from './datatext'
 function EartSphere(props) {
   const texture = useTexture('2k_earth_daymap.jpg')
   const earthRef2 = useRef()
-  useFrame((state, delta) => (earthRef2.current.rotation.y += delta * 0.1))
+  useFrame((state, delta) => (earthRef2.current.rotation.y += delta * props.rotateSphere))
   return (
-    <group ref={earthRef2}>
+    <group ref={earthRef2} rotation-y={props.rotationinitial}>
       <mesh scale={1.5}  >
         <sphereGeometry args={[1, 64, 64]} />
         <meshPhysicalMaterial map={texture} clearcoat={1} clearcoatRoughness={0} roughness={0} metalness={0.5} />
-        <Marker  rotation={[0, Math.PI / 2, 0]} position={[0, 1.3, 0]}>
-        <div style={{ position: 'absolute', fontSize: 14, letterSpacing: -0.5, left: 37.5, color: 'white' }}>north</div>
-          <FaMapMarkerAlt style={{ color: 'orange', scale:'3' }} />
+        <Marker rotation={[0, Math.PI / 2, 0]} position={[0, 1.3, 0]}>
+          <div style={{ position: 'absolute', fontSize: 14, letterSpacing: -0.5, left: 37.5, color: 'white' }}>north</div>
+          <FaMapMarkerAlt style={{ color: 'orange', scale: '3' }} />
         </Marker>
         <group position={[0, 0, 1.1]} rotation={[0, 0, Math.PI]}>
           <Marker rotation={[0, Math.PI / 2, Math.PI / 2]}>
-          <div style={{ position: 'absolute', fontSize: 14, letterSpacing: -0.5, left: 37.5, color:'white' }}>kraken base</div>
-            <FaGitkraken className='ikon' style={{ color: 'indianred', scale:'3' }} onClick={props.krakenclick} />
+            <div style={{ position: 'absolute', fontSize: 14, letterSpacing: -0.5, left: 37.5, color: 'white' }}>kraken base</div>
+            <FaGitkraken className='ikon' style={{ color: 'indianred', scale: '3' }} onClick={props.krakenclick} />
           </Marker>
         </group>
         <group position={[0, 0, -1.1]} rotation={[0, 0, Math.PI]}>
           <Marker rotation={[0, Math.PI / 2, Math.PI / 2]}>
-          <div style={{ position: 'absolute', fontSize: 14, letterSpacing: -0.5, left: 37.5, color:'white' }}>corsar group</div>
-            <FaGrunt className='ikon' style={{ color: 'indianred', scale:'3' }} onClick={props.gruntclick} />
+            <div style={{ position: 'absolute', fontSize: 14, letterSpacing: -0.5, left: 37.5, color: 'white' }}>corsar group</div>
+            <FaGrunt className='ikon' style={{ color: 'indianred', scale: '3' }} onClick={props.gruntclick} />
           </Marker>
         </group>
       </mesh>
@@ -42,7 +42,7 @@ function EartSphere(props) {
   )
 }
 function PlaneMoving() {
-  
+
   const earthRef2 = useRef()
   useFrame((state, delta) => (earthRef2.current.rotation.y += delta * 0.3))
   return (
@@ -60,10 +60,11 @@ function PlaneMoving() {
 
 
 export default function App() {
- 
-  const [visabilityDisplay,setVisabitityDisplay] = useState('hidden')
-  const [innerdatashow,setInnerdatashow] = useState(dataText[0])
 
+  const [visabilityDisplay, setVisabitityDisplay] = useState('hidden')
+  const [innerdatashow, setInnerdatashow] = useState(dataText[0])
+  const [rotateS, setRotateS] = useState(0.1)
+  const [rotationIni, setRotationIni] = useState(Math.PI / 2)
   return (
     <>
       <Canvas>
@@ -72,10 +73,22 @@ export default function App() {
         <Environment files="kloppenheim_02_puresky_1k_dark.hdr" background={true} blur={0.1} />
         <Suspense fallback={null}>
           <EartSphere position={[0, 0, 0]}
-           krakenclick={(e)=>{setVisabitityDisplay('visible'), setInnerdatashow(dataText[0])}}
-           gruntclick={(e)=>{setVisabitityDisplay('visible'),setInnerdatashow(dataText[1])}}  />
+            krakenclick={(e) => {
+              setVisabitityDisplay('visible'),
+                setInnerdatashow(dataText[0]),
+                setRotateS(0),
+                setRotationIni(0)
+            }}
+            gruntclick={(e) => {
+              setVisabitityDisplay('visible'),
+                setInnerdatashow(dataText[1]),
+                setRotateS(0),
+                setRotationIni(Math.PI)
+            }}
+            rotateSphere={rotateS}
+            rotationinitial={rotationIni} />
           <PlaneMoving position={[0, 0, 0]} />
-         
+
           <OrbitControls
             minAzimuthAngle={-Math.PI / 4}
             maxAzimuthAngle={Math.PI / 4}
@@ -84,10 +97,10 @@ export default function App() {
           <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
         </Suspense>
       </Canvas>
-       <Display isVisible={visabilityDisplay}
-        closeDisplay={(e)=>setVisabitityDisplay('hidden')}
+      <Display isVisible={visabilityDisplay}
+        closeDisplay={(e) => { setVisabitityDisplay('hidden'), setRotateS(0.1) }}
         content2={innerdatashow}
-        />
+      />
       <Loader />
     </>
   )
